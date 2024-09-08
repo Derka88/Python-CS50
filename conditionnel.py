c91fc20a3808bb0f239c23b278326d74a4366225
derka88 <kadermamoudou88@gmail.com>
2024-09-08 18:03:41 +0200

    boucle et condition

---
+++ conditionnel.py
@@ -0,0 +1,9 @@
+x = int(input("what's X? "))
+y = int(input("what's Y? "))
+
+if x > y :
+    print("x est supérieur à y")
+elif x < y :
+    print("x est inférieur à y")
+else :
+    print("x est égal à y")
